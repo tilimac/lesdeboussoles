268fbbe5460f6140b5dca236e4a3578d400f9134
tilimac <tilimac@gmail.com>
2016-07-14 03:45:24 +0200

    debug

--- src/FrontofficeBundle/Controller/DefaultController.php
+++ src/FrontofficeBundle/Controller/DefaultController.php
@@ -224,10 +224,11 @@ class DefaultController extends Controller
 
         $programme = array();
         foreach ($hikeManager->getAll() as $hike) {
-            if($hike->getDate() >= new \DateTime())
-            $month = $hike->getDate()->format('n');
-            $day = $hike->getDate()->format('j');
-            $programme[$month][$day] = $hike;
+            if($hike->getDate() >= new \DateTime()){
+                $month = $hike->getDate()->format('n');
+                $day = $hike->getDate()->format('j');
+                $programme[$month][$day] = $hike;
+            }
         }
 
         return array(
