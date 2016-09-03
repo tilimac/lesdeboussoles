b91495f7f84cd6d4ee0a9a506fd6556b9905976d
tilimac <tilimac@gmail.com>
2016-09-03 19:45:01 +0200

    fiche inscription

--- src/FrontofficeBundle/Entity/User.php
+++ src/FrontofficeBundle/Entity/User.php
@@ -10,6 +10,7 @@ namespace FrontofficeBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -25,14 +26,48 @@ class User extends BaseUser {
     protected $id;
 
     /**
-     * @ORM\Column(name="first_name", type="string", length=255, nullable=true)
+     * @ORM\Column(name="first_name", type="string", length=255)
      */
     private $firstName;
 
     /**
-     * @ORM\Column(name="last_name", type="string", length=255, nullable=true)
+     * @ORM\Column(name="last_name", type="string", length=255)
      */
     private $lastName;
+
+    /**
+     * @Assert\Range(
+     *      min = "-100 years",
+     *      max = "now"
+     * )
+     * @ORM\Column(name="birthdate", type="date")
+     */
+    private $birthdate;
+
+    /**
+     * @ORM\Column(name="adress1", type="string", length=255)
+     */
+    private $adress1;
+
+    /**
+     * @ORM\Column(name="adress2", type="string", length=255, nullable=true)
+     */
+    private $adress2;
+
+    /**
+     * @ORM\Column(name="zip_code", type="integer", length=5)
+     */
+    private $zipCode;
+
+    /**
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(name="phone", type="integer", length=10)
+     */
+    private $phone;
 
     public function __construct() {
         parent::__construct();
@@ -90,4 +125,141 @@ class User extends BaseUser {
         return $this->lastName;
     }
 
+    /**
+     * Set adress1
+     *
+     * @param string $adress1
+     * @return User
+     */
+    public function setAdress1($adress1)
+    {
+        $this->adress1 = $adress1;
+
+        return $this;
+    }
+
+    /**
+     * Get adress1
+     *
+     * @return string 
+     */
+    public function getAdress1()
+    {
+        return $this->adress1;
+    }
+
+    /**
+     * Set adress2
+     *
+     * @param string $adress2
+     * @return User
+     */
+    public function setAdress2($adress2)
+    {
+        $this->adress2 = $adress2;
+
+        return $this;
+    }
+
+    /**
+     * Get adress2
+     *
+     * @return string 
+     */
+    public function getAdress2()
+    {
+        return $this->adress2;
+    }
+
+    /**
+     * Set zipCode
+     *
+     * @param integer $zipCode
+     * @return User
+     */
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    /**
+     * Get zipCode
+     *
+     * @return integer 
+     */
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set birthdate
+     *
+     * @param \DateTime $birthdate
+     * @return User
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthdate
+     *
+     * @return \DateTime 
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param integer $phone
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return integer 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
 }
--- src/FrontofficeBundle/Form/Type/RegistrationFormType.php
+++ src/FrontofficeBundle/Form/Type/RegistrationFormType.php
@@ -7,9 +7,22 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationFormType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
+        /*$dateTimeNow = new \DateTime();
+        $yearMax = $dateTimeNow->format('Y');
+        $yearMin = $yearMax-100;*/
+
         $builder->add('firstName', 'text', array('label' => 'Prénom'))
-                ->add('lastName', 'text', array('label' => 'Nom'))
-                ->add('save', 'submit', array('label' => 'Enregistrer'));
+            ->add('lastName', 'text', array('label' => 'Nom'))
+            ->add('birthdate', 'birthday', array( 'label' => 'Date de naissance'))
+            ->add('adress1', 'text', array('label' => 'Adresse'))
+            ->add('adress2', 'text', array('label' => 'Complément d\'adresse'))
+            ->add('zipCode', 'integer', array('label' => 'Code postal'))
+            ->add('city', 'text', array('label' => 'Ville'))
+            ->add('accept', 'checkbox', array(
+                'label' => 'J\'accepte le règlement et la politique de confidentialité',
+                'mapped' => false
+            ))
+            ->add('save', 'submit', array('label' => 'Enregistrer'));
     }
 
     public function getParent() {
