4b44e3fd1730f4acee7cf6ac9d383d5f14403836
tilimac <tilimac@gmail.com>
2016-09-04 13:54:28 +0200

    formulaire inscription + debug

--- src/FrontofficeBundle/Form/Type/RegistrationFormType.php
+++ src/FrontofficeBundle/Form/Type/RegistrationFormType.php
@@ -23,9 +23,13 @@ class RegistrationFormType extends AbstractType {
             ->add('lastName', 'text', array('label' => 'Nom'))
             ->add('birthdate', 'birthday', array( 'label' => 'Date de naissance'))
             ->add('adress1', 'text', array('label' => 'Adresse'))
-            ->add('adress2', 'text', array('label' => 'Complément d\'adresse'))
+            ->add('adress2', 'text', array(
+                'label' => 'Complément d\'adresse',
+                'required' => false
+            ))
             ->add('zipCode', 'integer', array('label' => 'Code postal'))
             ->add('city', 'text', array('label' => 'Ville'))
+            ->add('phone', 'integer', array('label' => 'Téléphone'))
             ->add('accept', 'checkbox', array(
                 'label' => 'J\'accepte le règlement et la politique de confidentialité',
                 'mapped' => false
